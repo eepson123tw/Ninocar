cde9194ce54c9c61c315c68bc758a1b444364b01
allen <eepson123@gmail.com>
2021-02-16 20:58:42 +0800

    popovers done

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -5,35 +5,29 @@
 window.onload = function() {
     console.log(document.title);
 
-
     //popovers 
     let item = document.querySelector(`[data-js]`);
     let popoversArea = document.querySelector(`.popovers`);
 
-
-
     if (item) {
 
-        // item.classList.add('popovers');
-        item.addEventListener("mouseover", (e) => {
+        item.addEventListener("mouseenter", (e) => {
+
             e.preventDefault();
-            console.log(e);
             const { offsetLeft, offsetTop } = e.target;
-            popoversArea.style.top = `${offsetTop}px`;
-            popoversArea.style.left = `${offsetLeft}px`;
+            popoversArea.style.top = `${offsetTop - 50}px`;
+            popoversArea.style.left = `${offsetLeft - 280}px`;
             popoversArea.style.display = "block";
             popoversArea.style.opacity = 1;
         })
 
-        item.addEventListener("mouseout", (e) => {
-
-            // popoversArea.style.opacity = 0;
+        item.addEventListener("mouseleave", (e) => {
+            popoversArea.style.display = "none";
+            popoversArea.style.opacity = 0;
 
         })
 
     }
-
-
 
 
 
