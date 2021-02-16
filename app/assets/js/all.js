1dd6ea8c2ab59f51969e6e19b504dc532ad9d885
allen <eepson123@gmail.com>
2021-02-16 18:02:38 +0800

    popovers

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -2,9 +2,42 @@
 //     console.log('恭喜可以寫JQ囉');
 // });
 
-window.onload = function () {
+window.onload = function() {
     console.log(document.title);
 
+
+    //popovers 
+    let item = document.querySelector(`[data-js]`);
+    let popoversArea = document.querySelector(`.popovers`);
+
+
+
+    if (item) {
+
+        // item.classList.add('popovers');
+        item.addEventListener("mouseover", (e) => {
+            e.preventDefault();
+            console.log(e);
+            const { offsetLeft, offsetTop } = e.target;
+            popoversArea.style.top = `${offsetTop}px`;
+            popoversArea.style.left = `${offsetLeft}px`;
+            popoversArea.style.display = "block";
+            popoversArea.style.opacity = 1;
+        })
+
+        item.addEventListener("mouseout", (e) => {
+
+            // popoversArea.style.opacity = 0;
+
+        })
+
+    }
+
+
+
+
+
+
+
+
 };
-
-
