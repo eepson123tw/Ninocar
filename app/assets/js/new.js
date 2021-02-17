11adea231263352eb9aadc29e55a271e6ead5ce9
bensoen <marilyn780913@gmail.com>
2021-02-17 11:49:02 +0800

    no message

--- app/assets/js/new.js
+++ app/assets/js/new.js
@@ -1,3 +1,44 @@
-$(document).ready(function(){
-    console.log('恭喜可以寫JQ囉');
-});
+// $(document).ready(function(){
+//     console.log('恭喜可以寫JQ囉');
+// });
+
+
+var ary = [];
+for(let i = 0;i < 30;i++){
+    ary.push((i+1).toString());
+}
+//console.log(ary);
+
+function showBtn(){
+    var page = document.querySelector('.page_btn_nb');
+
+    var btnNum = Math.ceil(ary.length/10);
+    console.log(btnNum);
+
+    var str = '';
+    for(var i = 0; i < btnNum; i++){
+        str +=  `<span>${i + 1}</span>`
+    };
+    page.innerHTML = str;
+
+    //監聽按鈕
+    var btn = document.querySelectorAll('.page span')
+    for(var i = 0; i<btn.length; i++){
+        btn[i].addEventListener('click', changePage.bind(this,i+1,ary));
+    }
+}
+
+
+showBtn();
+
+function changePage(page,data){
+    var item = 10;
+    var pageIndexStart = (page - 1)*item
+    var pageIndexEnd = page * item
+    var str = '';
+    for(var i = pageIndexStart; i<pageIndexEnd; i++){
+        if (i>=data.length){break;}
+        str += `<div class="card">${data[i]}</div}`
+    };
+    document.querySelector('.card').innerHTML = str;
+}
