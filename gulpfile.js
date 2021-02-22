deed8bcbe7e34698a50d15cb44a9baf5eaf188e3
allen <eepson123@gmail.com>
2021-02-23 01:32:31 +0800

    修正 cartprogress bug

--- gulpfile.js
+++ gulpfile.js
@@ -75,7 +75,7 @@ exports.browser = function browsersync() {
         // browser: "chrome",
         server: {
             baseDir: './dist', //跟目錄設定
-            index: 'factory.html', //需更改成自己頁面的名稱
+            index: 'cart.html', //需更改成自己頁面的名稱
             injectChanges: false,
         },
     });
