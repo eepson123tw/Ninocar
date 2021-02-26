c0a59e877225f5f2c8750bef9329220d7ff20f6b
iko lin <chiwei991814@gmail.com>
2021-02-26 22:00:42 +0800

    增加增加vuex檔

---
+++ app/components/utils.js
@@ -0,0 +1,34 @@
+const store = new Vuex.Store({ //global store
+    state: { //類似 new Vue() 裡面的data
+        seriesList: [{
+            name: '工程系',
+            eName: 'EN',
+        }, {
+            name: 'RV休旅系',
+            eName: 'RV',
+        }, {
+            name: '計程車系',
+            eName: 'TA',
+        }, {
+            name: '巴士系',
+            eName: 'BS',
+        }, {
+            name: '警車系',
+            eName: 'PA',
+        }, {
+            name: '消防救護系',
+            eName: 'EM',
+        }, {
+            name: '轎車系',
+            eName: 'CA',
+        }, {
+            name: 'PREMIUM系',
+            eName: 'PM',
+        }],
+    },
+    mutations: { //類似 new Vue() 裡面的methods
+        addCount(state) {
+            state.count += 1;
+        },
+    }
+});
