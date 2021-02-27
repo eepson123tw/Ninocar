3f66f5f6a86175ad846ee2ef4f9bc492353cfa4e
Meitaku0519 <rustyperfume@gmail.com>
2021-02-27 13:08:39 +0800

    itemDetail頁 收放選單調整 RWD調整

--- app/assets/js/act.js
+++ app/assets/js/act.js
@@ -13,41 +13,39 @@ $(document).ready(function () {
     // console.log(lorem);
 
     //收放選單
-    // let angle = document.querySelector('.icon-down');
-    // let lorem = document.querySelector('.lorem');
+    let angle = document.querySelector('.icon-down');
+    let lorem = document.querySelector('.lorem');
 
-    // angle.onclick = function () {
-    //     lorem.classList.toggle('show');
-    //     lorem.classList.contains('show');
-    //     if (lorem.classList.contains('show')) {
-    //         angle.style.transform = 'rotate(180deg)';
-    //     } else {
-    //         angle.style.transform = 'rotate(0deg)';
-    //     }
-    // };
+    angle.onclick = function () {
+        lorem.classList.toggle('show');
+        lorem.classList.contains('show');
+        if (lorem.classList.contains('show')) {
+            angle.style.transform = 'rotate(180deg)';
+        } else {
+            angle.style.transform = 'rotate(0deg)';
+        }
+    };
 
-    // let angle2 = document.querySelector('.icon-down2');
-    // let lorem2 = document.querySelector('.lorem2');
+    let angle2 = document.querySelector('.icon-down2');
+    let lorem2 = document.querySelector('.lorem2');
 
-    // angle2.onclick = function () {
-    //     lorem2.classList.toggle('show');
-    //     lorem2.classList.contains('show');
-    //     if (lorem2.classList.contains('show')) {
-    //         angle2.style.transform = 'rotate(180deg)';
-    //     } else {
-    //         angle2.style.transform = 'rotate(0deg)';
-    //     }
-    // };
+    angle2.onclick = function () {
+        lorem2.classList.toggle('show');
+        lorem2.classList.contains('show');
+        if (lorem2.classList.contains('show')) {
+            angle2.style.transform = 'rotate(180deg)';
+        } else {
+            angle2.style.transform = 'rotate(0deg)';
+        }
+    };
 
-    $('.icon-down').click(function () {
-        $('.lorem')
-            .slideToggle()
-            .css(('transform', 'rotate(180deg)'));
-    });
+    // $('.icon-down').click(function () {
+    //     $('.lorem').slideToggle();
+    //     .css(('transform', 'rotate(180deg)'));
+    // });
 
-    $('.icon-down2').click(function () {
-        $('.lorem2')
-            .slideToggle()
-            .css(('transform', 'rotate(180deg)'));
-    });
+    // $('.icon-down2').click(function () {
+    //     $('.lorem2').slideToggle();
+    //     .css(('transform', 'rotate(180deg)'));
+    // });
 });
