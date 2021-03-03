036310c13febc5a44edd6157dcfb3c0bf07924df
tempura327 <cks6658324@yahoo.com.tw>
2021-03-03 22:59:30 +0800

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -92,7 +92,7 @@ exports.browser = function browsersync() {
     // browser: "chrome",
     server: {
       baseDir: './dist', //跟目錄設定
-      index: 'member.html', //需更改成自己頁面的名稱
+      index: 'factory.html', //需更改成自己頁面的名稱
       injectChanges: false,
     },
   });
