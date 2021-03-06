e6062e6b6ae46be43a696e2c1eb41c4582e70331
tempura327 <cks6658324@yahoo.com.tw>
2021-03-06 11:40:13 +0800

    修改取消配件時allIndex沒有跟著刪掉取消的配件的index問題、修改php SQL語法、要抓的項目

--- app/assets/php/getCustomizeProduct.php
+++ app/assets/php/getCustomizeProduct.php
@@ -3,14 +3,17 @@
   $workName = $_POST['workName'];
   $userNickName = $_POST['userNickName'];
   $inspiration = $_POST['inspiration'];
+  $workPrice = $_POST['workPrice'];
+  $workPostTime = $_POST['workPostTime'];
+  // $allIndex = $_POST['allIndex'];
 
   $Util = new UtilClass();
 
 
   if($workName){
 
-    // $sql = 'SELECT * FROM product';
-    $sql = 'INSERT INTO product(product_name, product_img, product_series, product_spec, product_seriesid, product_price, product_des, product_type) VALUES ($workName, "./assets/img/pic/model01.png", 8, 5, 1, 450, $inspiration, 0)';
+    // $sql = 'SELECT * FROM product'; //圖片位置先寫死的，需再改
+    $sql = 'INSERT INTO customize(product_id, board_id, member_id, product_name, product_img, product_price, product_des, product_type) VALUES ($allIndex, $workPostTime, ?, $workName, "./assets/img/pic/model.png", $workPrice, $inspiration, 0)';
 
     // $statement = $pdo->prepare($sql);
     $statement=$Util->getPDO()->prepare($sql);//目前只輸入一項資料($workName)
@@ -19,9 +22,3 @@
     // header('Location:./topic.php'); //跳轉到哪
   }
 ?>
-
-<!-- <html>
-<body>
-
-</body>
-</html> -->
