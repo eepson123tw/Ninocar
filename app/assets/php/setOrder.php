41e83208807cf8c9568790c5ad1e0ac8463f7bc6
allen <eepson123@gmail.com>
2021-03-07 23:31:56 +0800

    建立訂單與商品連結

--- app/assets/php/setOrder.php
+++ app/assets/php/setOrder.php
@@ -5,18 +5,21 @@
  $Util = new UtilClass();
    
 
-    
+    // 訂單區塊
     $ord_id =  $_POST['ORDER_ID']; 
     $mem_id =  $_POST['ID']; 
     $ord_cost =  $_POST['ORDER_COST']; 
     $ord_points =  $_POST['ORDER_POINTS']; 
     $ord_gain =  $_POST['ORDER_GAIN']; 
     $pay_type =  $_POST['PAY_TYPE']; 
+    $product_array = json_decode($_POST['PRODUCT_ARRAY']); 
+
+    // print_r($product_array);
+    // echo count($product_array);
 
     $sql = "INSERT INTO `team1`.`order`(order_id,member_id,order_cost,order_points,order_gain,
     pay_type,order_type,deliver_type,order_date,type_date) VALUES (?,?,?,?,?,?,0,0,Now(),Now())";
     
-
     $statement=$Util->getPDO()->prepare($sql);
     $statement->bindValue(1, $ord_id);
     $statement->bindValue(2, $mem_id);
@@ -27,15 +30,27 @@
     
     $statement->execute();
 
-    //抓出全部且依照順序封裝成一個二維陣列
-    $data = $statement->fetchAll();
  
+  
+    // 建立訂單與商品連結
+
+        for($i=0;$i<count($product_array);$i++){
+
+            $sql = "INSERT INTO `detail` (`order_id`, `product_id`) VALUES (?, ?)";
+    
+            $statement=$Util->getPDO()->prepare($sql);
+            $statement->bindValue(1, $ord_id);
+            $statement->bindValue(2, $product_array[$i]);
+            $statement->execute();
+        
+        };
+
 
 
 
 
     echo'新增成功';
-   
+
     
 
 
