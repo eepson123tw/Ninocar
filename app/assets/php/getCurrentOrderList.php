57485e7e3e66d1e4649d36a34eadaf711b1d5a30
allen <eepson123@gmail.com>
2021-03-13 21:48:52 +0800

    購買訂單完成

--- app/assets/php/getCurrentOrderList.php
+++ app/assets/php/getCurrentOrderList.php
@@ -9,13 +9,22 @@
     $data = json_decode(file_get_contents("php://input"));
     // print_r($data);
     $mem_id = $data -> memberId;
+    $order_id= $data -> orderId;
+
+  
+    $sql = "SELECT * FROM `order` as o JOIN detail as de
+    on o.order_id = de.order_id
+      JOIN product as pro 
+    on de.product_id = pro.product_id
+    WHERE o.member_id =? AND o.order_id =? ;";
 
 
-    $sql = "SELECT product_id FROM `detail` where order_id in ( SELECT order_id FROM `order` WHERE member_id = ?)";
+
     $statement = $Util->getPDO()->prepare($sql);
 
 
     $statement->bindValue(1, $mem_id);
+    $statement->bindValue(2, $order_id);
     $statement->execute();
     $data = $statement->fetchAll(PDO::FETCH_ASSOC);
     
