d42cc05d4f4fcbc6bf99c1ed52f34a33db1cd444
allen <eepson123@gmail.com>
2021-03-15 19:21:39 +0800

    修正手機版ＲＷＤ

--- app/assets/php/Lib/UtilClass.php
+++ app/assets/php/Lib/UtilClass.php
@@ -12,10 +12,18 @@ class UtilClass{
     //取得PDO物件
     function getPDO(){
         // $db_host = "127.0.0.1";
-        $db_host = "localhost";
-        $db_user = "root";
-        $db_pass = "password";
+        // $db_host = "localhost";
+        // $db_user = "root";
+        // $db_pass = "password";
+        // $db_select = "team1";
+        
+
+        // 將資料庫轉移至周杰的網域
+        $db_host = "10.2.0.202";
+        $db_user = "test";
+        $db_pass = "12345678";
         $db_select = "team1";
+
 
         //建立資料庫連線物件
         $dsn = "mysql:host=" . $db_host . ";dbname=" . $db_select;
